f81d936448699926881ff8fcc22cada90b3c838b
Marius <kazlausko@icloud.com>
2018-06-08 16:19:03 +0300

    Initial commit

---
+++ assets/javascript/gallery.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    $(function() {
+      $(".overlayer-wrapperlink").hover(function () {
+        $(this).toggleClass("is-caption-visible");
+      });
+    });
+
+  }); 
